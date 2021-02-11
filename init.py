b88750689a920a9512f4f1f5adf5d28637778869
Pietro Di Gangi <digangi@MBP-di-Pietro.station>
2021-02-11 10:40:47 +0100

    update 2021

--- init.py
+++ init.py
@@ -5,14 +5,18 @@ import matplotlib.dates as mdates
 import numpy as np
 import datetime
 from datetime import date, timedelta
+import matplotlib
+from matplotlib.lines import Line2D
+from matplotlib.patches import Patch
+import matplotlib.cm as cm
 
 params = {
     'font.family': 'serif',
-    'font.size' : 18, 'axes.titlesize' : 18, 'axes.labelsize' : 18, 'axes.linewidth' : 2,
+    'font.size' : 16, 'axes.titlesize' : 16, 'axes.labelsize' : 16, 'axes.linewidth' : 2,
     # ticks
-    'xtick.labelsize' : 18, 'ytick.labelsize' : 18, 'xtick.major.size' : 16, 'xtick.minor.size' : 8,
-    'ytick.major.size' : 16, 'ytick.minor.size' : 8, 'xtick.major.width' : 2, 'xtick.minor.width' : 2,
-    'ytick.major.width' : 2, 'ytick.minor.width' : 2, 'xtick.direction' : 'in', 'ytick.direction' : 'in',
+    'xtick.labelsize' : 16, 'ytick.labelsize' : 16, 'xtick.major.size' : 8, 'xtick.minor.size' : 4,
+    'ytick.major.size' : 8, 'ytick.minor.size' : 4, 'xtick.major.width' : 1, 'xtick.minor.width' : 1,
+    'ytick.major.width' : 1, 'ytick.minor.width' : 1, 'xtick.direction' : 'in', 'ytick.direction' : 'in',
     # markers
     'lines.markersize' : 12, 'lines.markeredgewidth' : 3, 'errorbar.capsize' : 8, 'lines.linewidth' : 3,
     #'lines.linestyle' : None, 'lines.marker' : None,
@@ -30,5 +34,21 @@ params = {
     'axes.facecolor': 'white'   # axes background color
 }
 plt.rcParams.update(params)
+
+from matplotlib import rcParams
+rcParams['font.family'] = 'sans-serif'
+rcParams['font.sans-serif'] = 'PF Din Text Cond Pro'
+#rcParams['font.sans-serif'] = 'Exo-2'
+rcParams['font.size'] = 20
+rcParams['font.weight'] = '500'
+rcParams['axes.labelsize'] = 20
+rcParams['axes.labelweight'] = '700'
+rcParams['axes.titleweight'] = '700'
+rcParams['axes.titlesize'] = 22
+rcParams['xtick.labelsize'] = 20
+rcParams['ytick.labelsize'] = 20
+rcParams['legend.fontsize'] = 20
+rcParams['figure.titlesize'] = 24
+
 today = datetime.datetime.now()
 print('Current time:', today)
